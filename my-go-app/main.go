bcd432131e4c8dcf857cecc7f6c72748fca145e0
binayrawat <binayrawat8@gmail.com>
2024-10-15 22:45:49 +0200

    finn

--- my-go-app/main.go
+++ my-go-app/main.go
@@ -1,17 +1,18 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "github.com/gorilla/mux"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-    r := mux.NewRouter()
-    r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintln(w, "Hello, World!")
-    })
+	r := mux.NewRouter()
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello, World1!")
+	})
 
-    http.Handle("/", r)
-    http.ListenAndServe(":8080", nil)
+	http.Handle("/", r)
+	http.ListenAndServe(":8080", nil)
 }
